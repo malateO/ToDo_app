16040772475aef6879eddcf7bd57a09c466fd972
Oliver Malate <oliver95malate@gmail.com>
2024-06-09 04:39:02 +0800

    display list in the main element IIP

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+// modal variables
 const openModalBtn = document.querySelectorAll("[data-modal-open]");
 const closeModalBtn = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
 const submitTaskBtn = document.getElementById("submit-task");
+// input variables
+const titleElement = document.getElementById("task-title");
+const dateElement = document.getElementById("task-date");
+const descElement = document.getElementById("task-description");
+// todo list content variable
+const taskList = document.querySelector(".task-list-content");
 
+const todoList = [];
+
+// get the value in the forms
+// save to local storage
+const submitTask = () => {
+  const todoDetails = {
+    title: titleElement.value,
+    date: dateElement.value,
+    description: descElement.value,
+  };
+  todoList.push(todoDetails);
+  console.log(todoList);
+  todoList.forEach(({ title, date, description }) => {
+    taskList.innerHTML = `<p>${title}</p><p>${date}</p><p>${description}</p>`;
+  });
+};
+
+// display in the todo list content
+
+// functions for modal
 openModalBtn.forEach((button) => {
   button.addEventListener("click", () => {
     const modal = document.querySelector(button.dataset.modalOpen);
@@ -22,9 +49,10 @@ overlay.addEventListener("click", () => {
   modals.forEach((modal) => closeModal(modal));
 });
 
-submitTaskBtn.addEventListener("click", () => {
-  const modals = document.querySelectorAll(".modal.active");
-  closeModal(modals);
+// submit button animation IP
+submitTaskBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  submitTask();
 });
 
 function openModal(modal) {
