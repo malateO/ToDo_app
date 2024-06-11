be12f2575d1d39c8280bc9aea7f35694df5e8a1a
Oliver Malate <oliver95malate@gmail.com>
2024-06-12 04:24:43 +0800

    innerHTML design finished

--- script.js
+++ script.js
@@ -32,12 +32,19 @@ const displayTask = () => {
   taskList.innerHTML = "";
   // display in the todo list content
   todoList.forEach(({ title, date, description }) => {
-    taskList.innerHTML += `<div class = "todo-list-cont-js"><p class="todo-list-p-js">Title: <span class = "todo-list-js">${title}</span></p>
-    <p class="todo-list-p-js">Date: <span class = "todo-list-js">${date}</span></p>
-    <p class="todo-list-p-js">Description: <span class = "todo-list-js">${description}</span></p>
-    <button>Update</button>
-    <button>Delete</button>
-    </div>`;
+    taskList.innerHTML += `
+    <div class = "todo-list-cont-js">
+      <div class = "todo-list-js-pdiv">      
+        <p class="todo-list-p-js">Title: <span class = "todo-list-js">${title}</span></p>
+        <p class="todo-list-p-js">Date: <span class = "todo-list-js">${date}</span></p>
+        <p class="todo-list-p-js">Description: <span class = "todo-list-js">${description}</span></p>
+      </div>
+      <div class = "todo-list-js-btndiv">
+        <button class = "todo-list-btn-js update">Update</button>
+        <button class = "todo-list-btn-js delete">Delete</button>
+      </div>
+    </div>
+    `;
   });
 };
 
@@ -63,6 +70,8 @@ overlay.addEventListener("click", () => {
 
 submitTaskBtn.addEventListener("click", (e) => {
   if (titleElement.value == "" || dateElement.value == "") {
+    const overlay = document.getElementById("overlay");
+    const modal = document.getElementById("modal");
     modal.classList.add("active");
     overlay.classList.add("active");
   } else {
